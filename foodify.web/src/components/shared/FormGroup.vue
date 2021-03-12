725abf5cb05c1c199069a5d03ee6bfdd936ac2ef
Senn <severin@senn-online.ch>
2021-03-12 09:55:51 +0100

    vuelidate error messages bug fixed

--- foodify.web/src/components/shared/FormGroup.vue
+++ foodify.web/src/components/shared/FormGroup.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
     <slot
-      :attrs="{ error: hasErrors, 'error-message': firstErrorMessage }"
+      :attrs="{ errorMessages: activeErrorMessages, success: isValid }"
+      :hasErrors="hasErrors"
     />
   </div>
 </template>
 <script>
 import { singleErrorExtractorMixin } from "vuelidate-error-extractor";
 export default {
-  name: "FormGroup",
   extends: singleErrorExtractorMixin,
-  inheritAttrs: false,
 };
 </script>
