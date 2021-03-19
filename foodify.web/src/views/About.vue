58b31912b68a9c8c9a61e82b0d75a5a4d9bd997a
noahbr62 <noah.barth@edu.tbz.ch>
2021-03-19 10:52:04 +0100

    Started with the About-Page and moaaare

--- foodify.web/src/views/About.vue
+++ foodify.web/src/views/About.vue
@@ -1,8 +1,18 @@
 <template>
   <v-main>
-  <v-app-bar app color="primary">What is Foodify?</v-app-bar>
+  <v-app-bar app color="primary">About</v-app-bar>
     <v-container>
-
+      <v-card color="white" class="black--text mb-3 ml-5" >
+        <v-card-title class="primary  white--text">What is Foodify?</v-card-title>
+        <v-card-text class="black--text">Foodify is a management-tool for all your fridge-related needs. With Foodify you can
+        track any changes to your foodstock and quickly analyze, which
+        </v-card-text>
+      </v-card>
+      <v-spacer></v-spacer>
+      <v-card color="white" class="black--text">
+        <v-card-title class="primary white--text">Why foodify?</v-card-title>
+        <v-card-text class="black--text">Foodify is a management-tool for all your fridge-related needs</v-card-text>
+      </v-card>
     </v-container>
   </v-main>
 </template>
