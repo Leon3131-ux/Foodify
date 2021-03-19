af06afcd43b9f854aeb4064c2d4570bfefb54d0a
Leon Domiter <leon.domiter@hotmail.com>
2021-03-19 09:40:56 +0100

    added webconfig

--- backend/src/main/java/com/foodifyinc/demo/config/WebConfig.java
+++ backend/src/main/java/com/foodifyinc/demo/config/WebConfig.java
@@ -1,7 +1,20 @@
 package com.foodifyinc.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH")
+                .allowedHeaders("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true)
+                .exposedHeaders("Authorization");
+    }
+
 }
