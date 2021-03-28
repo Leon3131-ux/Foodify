bc29b20f0e6799de16f881ac690e2bf98612e78b
Noah Barth <noah.barth@edu.tbz.ch>
2021-03-28 19:53:32 +0200

    Work on About page

--- foodify.web/src/views/About.vue
+++ foodify.web/src/views/About.vue
@@ -5,12 +5,30 @@
         <v-card-title class="primary white--text"
           >What is Foodify?</v-card-title
         >
-        <v-card-text class="black--text"
-          >Foodify is a management-tool for all your fridge-related needs. With
-          Foodify you can track any changes to your foodstock and quickly
-          analyze, which
-        </v-card-text>
+        <v-row>
+          <v-col
+              cols="6"
+          >
+            <v-card-text
+                class="black--text"
+                width="50%"
+              >Foodify is a management-tool for all your fridge-related needs. With
+              Foodify you can track any changes to your foodstock and quickly
+              analyze, which
+            </v-card-text>
+          </v-col>
+          <v-col
+              cols="6">
+            <v-img
+                class="mt-3 mr-3"
+                col
+                float="right"
+                src="https://www.kuechen-staude.de/wp-content/uploads/2020/02/magazin-startseite-2560x1440-kuehlschrank-a-z.jpg"
+            ></v-img>
+          </v-col>
+        </v-row>
       </v-card>
+
       <v-spacer></v-spacer>
       <v-card color="white" class="black--text">
         <v-card-title class="primary white--text">Why foodify?</v-card-title>
