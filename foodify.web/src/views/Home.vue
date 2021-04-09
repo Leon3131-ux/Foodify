f97ba87c345c8165722e903781716840eb9d0382
Noah Barth <noah.barth@edu.tbz.ch>
2021-04-09 10:02:53 +0200

    Work on About page

--- foodify.web/src/views/Home.vue
+++ foodify.web/src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
   <v-main> </v-main>
 </template>
-
+  <v-carousel>
+  <v-carousel-item
+      v-for="(sliderItems,i) in sliderItems"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+  ></v-carousel-item>
+  </v-carousel>
 
 <script>
 import { required } from "vuelidate/lib/validators";
@@ -18,7 +26,22 @@ export default {
   components: {},
   async mounted() {},
   data() {
-    return {};
+    return {
+      sliderItems: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+      ],
+    };
   },
   methods: {},
 };
